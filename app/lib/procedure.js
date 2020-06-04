8b6e72c76db4b95c614f955cdbccf9223c4a73a9
Thirteentheye <gjshapymaher@gmail.com>
2020-06-04 22:47:33 +0800

    fix compatibility: compatible to node version 8

--- app/lib/procedure.js
+++ app/lib/procedure.js
@@ -3,8 +3,10 @@
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
+const semver = require('semver');
 
 const readdirAsync = util.promisify(fs.readdir);
+const statAsync = util.promisify(fs.stat);
 
 module.exports.getBootPaths = function(config, baseDir) {
   const paths = Array.isArray(config.paths) ? config.paths : [ String(config.paths) ];
@@ -14,17 +16,7 @@ module.exports.getBootPaths = function(config, baseDir) {
 module.exports.getFlattenedFilePaths = async function(bootPaths) {
   const readyFilePaths = [];
   for (const bootPath of bootPaths) {
-    readyFilePaths.push(readdirAsync(bootPath, { withFileTypes: true })
-      .then(dirents => {
-        const filePaths = [];
-        for (const dirent of dirents) {
-          if (dirent.isFile()) {
-            filePaths.push(path.join(bootPath, dirent.name));
-          }
-        }
-        return filePaths;
-      }).catch(() => [])
-    );
+    readyFilePaths.push(getDirFilePaths(bootPath));
   }
   const filePaths = await Promise.all(readyFilePaths);
   return filePaths.reduce((p, c) => p.concat(c), []);
@@ -34,7 +26,7 @@ module.exports.loadOrderedScripts = function(bootFiles, processer) {
   const scripts = [];
   for (const bootFile of bootFiles) {
     const bootScript = require(bootFile);
-    let script;
+    let script = null;
     if (typeof bootScript === 'function') {
       script = bootScript(processer);
     }
@@ -64,6 +56,24 @@ module.exports.runScripts = async function(scripts, pname, stage) {
   }
   await Promise.all(batch);
 };
+
+function getDirFilePaths(bootPath) {
+  if (semver.gte(process.version, '10.10.0')) {
+    return readdirAsync(bootPath, { withFileTypes: true })
+      .then(dirents => dirents.filter(d => d.isFile()).map(d => path.join(bootPath, d.name)))
+      .catch(() => []);
+  }
+  return readdirAsync(bootPath)
+    .then(async files => {
+      const readyStats = [];
+      for (const file of files) {
+        const filePath = path.join(bootPath, file);
+        readyStats.push(statAsync(filePath).then(stat => (stat.isFile() ? filePath : '')));
+      }
+      const filePaths = await Promise.all(readyStats);
+      return filePaths.filter(fp => fp);
+    }).catch(() => []);
+}
 
 function deduplicate(array) {
   return [ ...new Set(array) ];
